d57a37301ac7131d93d34b190d9942d25b5030cc
geon <lkcbap12@gmail.com>
2024-09-25 22:24:46 +0900

    5-lecture apply

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,7 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
@@ -25,6 +26,22 @@ public class SingletonTest {
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
 
     }
+
+    @Test
+    @DisplayName("스프링 컨테이너, 싱글톤")
+    void springContainer(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService",MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService",MemberService.class);
+
+        // 참조값 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // 같은지 확인
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+    }
+
 
     @Test
     @DisplayName("싱글톤 패턴을 적용한 객체 사용")
