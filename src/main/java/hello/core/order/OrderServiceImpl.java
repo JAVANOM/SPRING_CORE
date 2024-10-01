3118df9cb7cdf26d6b291dd967e5dfaccb80c622
lkcba <lkcbap12@gmail.com>
2024-10-01 23:59:11 +0900

    6-lecture apply

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,17 +6,37 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
-
-    private final MemberRepository memberRepository;
+    // 필드 주입
+    @Autowired
+    private MemberRepository memberRepository;
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    private final DiscountPolicy discountPolicy;
+    @Autowired
+    private DiscountPolicy discountPolicy;
 
+    // 수정자 주입(선택적, 변경 가능성)
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+         this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+
+
+    //public OrderServiceImpl() {
+    //}
+
+    // 생성자 주입 방법(불변, final)
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
