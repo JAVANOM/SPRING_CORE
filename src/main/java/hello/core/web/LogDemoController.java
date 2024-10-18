2cdf3fe3b1695fe6f1f2990cb01d5ba6d2d7a265
lkcba <lkcbap12@gmail.com>
2024-10-18 22:38:53 +0900

    9-lecture apply

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -21,6 +21,7 @@ public class LogDemoController {
     public String logDemo(HttpServletRequest request) {
         // MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
+
         System.out.println("myLogger: " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
